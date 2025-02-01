542c8ead4dc85667def34136957a4273fc38c222
Luis Cesar Rodrigues Gonçalves <51440316+LuisCesarPhoenix@users.noreply.github.com>
2025-02-01 19:01:52 -0300

    Update server.js

--- api-crud-mysql/server.js
+++ api-crud-mysql/server.js
@@ -1,76 +1,70 @@
+process.on('uncaughtException', (err) => {
+    console.error('Erro não tratado:', err);
+});
 /* Log de erros detalhados: Caso o erro persista, você pode adicionar um log detalhado para depuração.
 Isso ajudará a identificar erros silenciosos que possam estar ocorrendo.
 */
-process.on('uncaughtException', (err) => {
-    console.error('Erro não tratado:', err);
-  });
-  
 
+require('dotenv').config(); // Carregar variáveis de ambiente
+const express = require('express'); // Importa o Express
 /* Essas duas linhas são usadas para configurar e importar as dependências necessárias para o seu servidor
  em Node.js 
 Com essas duas linhas, você prepara seu ambiente para:
 1 - Carregar variáveis de ambiente de um arquivo .env (útil para configuração segura).
 2 - Iniciar a criação de um servidor com o framework Express.
-*/
-require('dotenv').config(); // Carregar variáveis de ambiente
-const express = require('express'); // Importa o Express
-/* dotenv: Esse módulo carrega variáveis de ambiente a partir de um arquivo .env para dentro da sua 
+
+dotenv: Esse módulo carrega variáveis de ambiente a partir de um arquivo .env para dentro da sua 
 aplicação. O método config() lê o arquivo .env e adiciona as variáveis de ambiente ao process.env. 
 Isso permite que você armazene informações sensíveis, como senhas de banco de dados, chaves de API 
-e configurações de ambiente, de forma segura.
-Dessa forma, você pode acessar essas variáveis no código com process.env.NOME_DA_VARIAVEL.
+e configurações de ambiente de forma segura.
+Dessa forma, você pode acessar essas variáveis no código com process.env.NOME_DA_VARIAVEL .
 express: Este comando importa o módulo Express para a sua aplicação. O Express é um framework web 
 para Node.js que facilita a criação de APIs e servidores. Ao importar o Express, você pode criar rotas, 
 configurar middlewares, manipular requisições e respostas, entre outras funcionalidades.
 */
 
+const userRoutes = require('./src/routes/userRoutes'); // Importa as rotas de usuário
+// Importa as rotas definidas no arquivo 'userRoutes.js' dentro da pasta 'src/routes'
+
 const connection = require('./src/config/database'); // Conexão MySQL
 
-
+const app = express(); // Inicializa o servidor Express
 /*Aqui você está criando uma instância do Express, que é o framework para aplicações web em Node.js. 
 Isso inicializa o servidor que vai gerenciar as rotas e requisições.
 */
-const app = express(); // Inicializa o servidor Express
 
-
+const PORT = process.env.PORT || 3000; // Define a porta
 /*A variável PORT pega a porta do ambiente (se definida) ou usa a porta 3000 como padrão. Isso permite 
 que você altere a porta facilmente, dependendo do ambiente onde a aplicação está sendo executada 
 (como desenvolvimento ou produção).
 */
-const PORT = process.env.PORT || 3000; // Define a porta
 
-
+app.use(express.json()); // Middleware para interpretar JSON. Habilita JSON no corpo das requisições
 /*Esta linha usa o middleware do Express para interpretar requisições com o corpo em formato JSON. 
 Isso é necessário para trabalhar com APIs que enviam ou recebem dados JSON.
 */
-app.use(express.json()); // Middleware para interpretar JSON
 
+app.use('/api/users', userRoutes); 
+// Define que todas as requisições para '/api/users' serão tratadas pelas rotas importadas
 
-/* rota get "/lista" que fará uma listagem de tarefas adicionadas
-exemplo: 
-A resposta deve conter uma array de objetos 
-Um array, em programação, é uma estrutura de dados que armazena uma coleção ordenada de elementos. Cada elemento possui uma posição numérica chamada índice, que começa em 0.
-Exemplos mais variados de arrays simples 
-*/
-
-
-// Declarar os arrays e objetos para teste
-/* const numeros = [1, 2, 3, 4, 5]; // Array de números
-const misto = [10, "Olá", true, null, { nome: "João" }]; // Array misto (pouco comum, mas demonstra a flexibilidade)
-*/
 const frutas = ['Maçãs', 'Bananas', 'Leite']; // Array de strings 
 const produtos = [ // Array de Objetos
   { produto: 'TV', valor: 'R$ 3000' },
   { produto: 'Geladeira', valor: 'R$ 2500' }
 ];
+/* Um array, em programação, é uma estrutura de dados que armazena uma coleção ordenada de elementos. Cada elemento possui uma posição numérica 
+chamada índice, que começa em 0. 
 
-// Rota GET para listar frutas
-app.get('/api/frutas', (req, res) => {
+Declarar os arrays e objetos para teste
+const numeros = [1, 2, 3, 4, 5]; // Array de números
+const misto = [10, "Olá", true, null, { nome: "João" }]; // Array misto (pouco comum, mas demonstra a flexibilidade)
+rota get "/lista" que fará uma listagem de tarefas adicionada
+*/
+app.get('/api/frutas', (req, res) => { // Rota GET para listar frutas
   res.json({ frutas });
 });
 
-// Rota GET para listar produtos
-app.get('/api/produtos', (req, res) => {
+app.get('/api/produtos', (req, res) => { // Rota GET para listar produtos
   res.json({ produtos });
 });
 
@@ -106,30 +100,25 @@ Resposta esperada (JSON):
 }
 */
 
-
+app.get('/', (req, res) => {
+  res.json({ message: 'Olá Mundo!' });
+  });
 /* Rota básica
 Aqui você está criando uma rota GET na raiz (/). Quando a rota é acessada, a resposta será um objeto 
 JSON com a chave message e o valor "Olá Mundo!". O código de status HTTP 200 indica que a requisição 
 foi bem-sucedida.
-app.get('/', (req, res) => {
-res.json({ message: 'Olá Mundo!' });
+app.get("/", function(req,res) {
+    res.status(200).json({message:"Olá Mundo!"});
 });
 Teste no navegador ou Insomnia
 URL: http://localhost:3000/
 */
-app.get("/", function(req,res) {
-    res.status(200).json({message:"Olá Mundo!"});
-  });
 
-
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
 /* Inicia o servidor
 Essa linha faz o servidor "escutar" a porta definida na variável PORT. Quando o servidor estiver rodando, 
 ele exibirá no console a mensagem indicando que está ativo e rodando na URL configurada 
 (http://localhost:3000 ou outra porta, dependendo do ambiente).
 */
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
-
-
-        
