d016b16fd53df8cc4e9c27acf1264b574087667b
Luis Cesar Rodrigues Gonçalves <51440316+LuisCesarPhoenix@users.noreply.github.com>
2025-02-06 14:32:27 -0300

    Update migrationRoutes.js

--- api-crud-mysql/src/routes/migrationRoutes.js
+++ api-crud-mysql/src/routes/migrationRoutes.js
@@ -1,11 +1,9 @@
 // src/routes/migrationRoutes.js (Rota para migração)
-// Chama o controller de migração.
-const express = require('express');
-const { migrateData } = require('../controllers/migrationController');
-
+// Chama o controller de migração. Define a rota para iniciar a migração.
+const express = require("express");
 const router = express.Router();
+const { migrateData } = require("../controllers/migrationController");
 
-router.post('/migrate', migrateData);
-// router.post('/', migrateData);
+router.post("/migrate", migrateData);
+
 module.exports = router;
-
